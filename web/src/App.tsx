c81ec52a90ac5ff4ce9f1bf1dc50b2c1a6b3171c
Mylena Verspeelt <mylena49@hotmail.com>
2022-05-03 09:15:43 -0300

    useState do widget

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,8 @@
+import { Widget } from "./components/Widget";
 
 // interface ButtonProps{
 //     text?: string;
-// } aqui é o typescript, vc ta determinando que o tipo de props que o botao recebe é do tipo string. a interrogação permite que se crie elementos sem props definidas, porem ela vai ter um valor default se nao for passado nenhuma props. caso nao tenha uma ? nao da pra criar esse componente sem props. 
-
-import { Widget } from "./components/Widget";
+// } aqui é o typescript, vc ta determinando que o tipo de props que o botao recebe é do tipo string. a interrogação permite que se crie elementos sem props definidas, porem ela vai ter um valor default(linha 7) se nao for passado nenhuma props. caso nao tenha uma ? nao da pra criar esse componente sem props. 
 
 // function Button(props: ButtonProps){
 // return <button>{props.text ?? "Default"}</button>
@@ -21,7 +20,7 @@ import { Widget } from "./components/Widget";
 // }
 
 // export default App
-  
+
 
 export function App(){
     return <Widget />
